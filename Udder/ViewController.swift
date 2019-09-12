fc2f46debb7427500bd0b5813e2526449c051633
Mohammed Lubbad <engmlubbad@gmail.com>
2019-09-12 15:45:50 +0300

    New change

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidDisappear(_ animated: Bool) {
+    
+    }
 
 }
 
